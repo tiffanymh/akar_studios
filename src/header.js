4ddcd96dc96e96e71f0005548b1c2ca8c55550fb
tiffanymh <tiffanymadeline.hartono@gmail.com>
2023-07-03 17:55:50 -0700

    initial setup

---
+++ src/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <div className="about-us">Shop men</div>
+        <div className="shop">Shop women</div>
+
+        <h1 className="h-1">Akar Studios</h1>
+
+        <div className="IDR">IDR</div>
+        <div className="my-account">My Account</div>
+        <div className="cart">Cart (0)</div>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
