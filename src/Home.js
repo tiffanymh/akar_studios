25eac282017645f9486cf3d64d2726c065354272
tiffanymh <tiffanymadeline.hartono@gmail.com>
2023-07-03 21:43:25 -0700

    header component

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,11 @@
 import React from "react";
 import Header from "./header";
-import "./home.css";
+import './fonts/fonts.css';
 
 const Home = () => {
   return (
-      <div className="box">
-          <div className="landing-page-wrapper">
-              <div className="landing-page">
-                <Header />
-                  <div className="looks-by-you">
-                      <img className="rectangle" alt="Rectangle" src="rectangle-6.png" />
-                      <img className="img" alt="Rectangle" src="rectangle-7.png" />
-                      <img className="rectangle-2" alt="Rectangle" src="rectangle-8.png" />
-                      <img className="rectangle-3" alt="Rectangle" src="rectangle-9.png" />
-                      <img className="rectangle-4" alt="Rectangle" src="rectangle-10.png" />
-                      <div className="best-sellers">Best Sellers</div>
-                  </div>
-                  <p className="mid-year-sale-off">Mid Year Sale: 50% Off + Free Shipping Indonesia + Usa</p>
-                  <p className="p">Mid Year Sale: 50% Off + Free Shipping Indonesia + Usa</p>
-                  <p className="mid-year-sale-off-2">Mid Year Sale: 50% Off + Free Shipping Indonesia + Usa</p>
-                  <img className="img-2" alt="Img" src="9179844ac7029f8cfbcef5e8684a9f41-1.png" />
-              </div>
-          </div>
+      <div>
+        <Header />
       </div>
   );
 };
--- src/header.js
+++ src/header.js
@@ -1,19 +1,61 @@
 import React from "react";
 
+import Grid from '@mui/material/Grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        align: "center",
+        color: "black",
+        fontFamily: "Work Sans",
+        fontSize: "20px",
+        fontWeight: "400",
+      }
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "white",
+          boxShadow: "none",
+        }
+      }
+    }
+  }
+})
+
 function Header() {
   return (
-    <div>
-      <header className="header">
-        <div className="about-us">Shop men</div>
-        <div className="shop">Shop women</div>
-
-        <h1 className="h-1">Akar Studios</h1>
-
-        <div className="IDR">IDR</div>
-        <div className="my-account">My Account</div>
-        <div className="cart">Cart (0)</div>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container spacing={2}>
+            <Grid item>
+              <Typography> About Us </Typography>
+            </Grid>
+            <Grid item>
+              <Typography> Shop </Typography>
+            </Grid>
+            <Grid item md={true}>
+              <Typography sx={{ fontFamily: "Playfair Display", fontWeight: "700", fontSize: "60px"}}> 
+              Akar Studios
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography> IDR </Typography>
+            </Grid>
+            <Grid item>
+              <Typography> My Account </Typography>
+            </Grid>
+            <Grid item>
+              <Typography> Cart (0) </Typography>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </ThemeProvider>
   )
 }
 
